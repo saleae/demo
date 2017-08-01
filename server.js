280518c8970a54d7ff966d073039a0ca4ff307d6
Marcus10110 <markgarrison@saleae.com>
2017-08-01 12:57:23 -0700

    added sample db code.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+// var mongo = require('mongodb').MongoClient;
+// var mysql = require('mysql');
 
 // Get our API routes
 const api = require('./server/routes/api');
@@ -23,6 +25,36 @@ app.use('/api', api);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+/*
+var mongo_url = 'mongodb://u9nk7zFl225V:GzfZkYr5tDfa@ds153392.mlab.com:53392/saleae_sandbox';
+mongo.connect(mongo_url, function(err, db) {
+  if(!err){
+    console.log("Connected successfully to mongo database");
+    db.close();
+  }
+});
+*/
+/*
+// hosted SQL not currently availible. To use MySQL, run locally and place your connection information here
+var connection = mysql.createConnection({
+  host     : 'sql3.freemysqlhosting.net',
+  user     : 'sql3184678',
+  password : '8KvEnV3c93',
+  database : 'sql3184678'
+});
+
+connection.connect(function(err) {
+  if (!err) {
+    console.log('connected as id ' + connection.threadId);
+  }else
+  {
+      console.error('error connecting: ' + err.stack);
+  }
+});
+*/
+
+
 
 /**
  * Get port from environment and store in Express.
